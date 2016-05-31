af0070c2c344523275ce56299b384f73f9a1ae19
Ellen Mey <eisforellen@gmail.com>
2016-05-31 15:42:50 -0400

    Initial commit with XCode project

---
+++ Hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hangman
+//
+//  Created by Ellen Mey on 5/31/16.
+//  Copyright © 2016 Ellen Mey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
